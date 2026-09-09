d1b047c26a79575820f20b875ad09ba923b52d5b
Justintunsday <162324843+Justintunsday@users.noreply.github.com>
2026-09-09 23:04:10 +0800

    Align iOS package targets and framework links

--- Packages/RimeKit/Package.swift
+++ Packages/RimeKit/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "RimeKit",
   platforms: [
-    .iOS(.v15),
+    .iOS(.v16),
   ],
   products: [
     .library(name: "RimeKit", targets: ["RimeKit"]),
