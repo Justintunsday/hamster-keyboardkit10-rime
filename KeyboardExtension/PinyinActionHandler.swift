0499b95679faaa767467a013e841a0b4251505cd
Justintunsday <162324843+Justintunsday@users.noreply.github.com>
2026-09-12 16:58:14 +0800

    Build and retain unsigned IPA

--- KeyboardExtension/PinyinActionHandler.swift
+++ KeyboardExtension/PinyinActionHandler.swift
@@ -16,7 +16,18 @@ final class PinyinActionHandler: StandardKeyboardActionHandler {
         self.applyTransition = applyTransition
         self.hostController = controller
         self.lastKeyboardType = controller.state.keyboardContext.keyboardType
-        super.init(controller: controller)
+        super.init(
+            controller: controller,
+            keyboardContext: controller.state.keyboardContext,
+            keyboardBehavior: controller.services.keyboardBehavior,
+            autocompleteContext: controller.state.autocompleteContext,
+            autocompleteService: controller.services.autocompleteService,
+            emojiContext: controller.state.emojiContext,
+            feedbackContext: controller.state.feedbackContext,
+            feedbackService: controller.services.feedbackService,
+            keyboardAppContext: controller.state.appContext,
+            spacebarDragGestureHandler: controller.services.spacebarDragGestureHandler
+        )
     }
 
     override func handle(_ gesture: Keyboard.Gesture, on action: KeyboardAction) {
