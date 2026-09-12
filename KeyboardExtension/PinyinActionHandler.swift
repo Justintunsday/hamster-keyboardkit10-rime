d35cb58f0459aa9761c31cab77e92a2c6cb476c4
Justintunsday <162324843+Justintunsday@users.noreply.github.com>
2026-09-12 13:37:12 +0800

    Use current KeyboardKit action handler namespace

--- KeyboardExtension/PinyinActionHandler.swift
+++ KeyboardExtension/PinyinActionHandler.swift
@@ -1,7 +1,7 @@
 import KeyboardKit
 import PinyinCore
 
-final class PinyinActionHandler: KeyboardAction.StandardKeyboardActionHandler {
+final class PinyinActionHandler: StandardKeyboardActionHandler {
     private let session: KeyboardSession
     private let applyTransition: (PinyinTransition) -> Void
     private weak var hostController: KeyboardInputViewController?
